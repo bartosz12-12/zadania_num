e2758185df811a343d84f6545d39dfbecdca9e8c
bartek <bartek.fi@interia.pl>
2021-04-14 17:58:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# #zad1
+# a= np.arange(4,4*21,4)
+# print(a)
+
+# #zad2
+# b = np.array([1.2,3.14,5.99,1.11])
+# print(b)
+# print(b.dtype)
+# c = np.array(b, dtype='int32')
+# print(c)
+# print(c.dtype)
+
+# # #zad3
+# def funkcja(n : int):
+#     d = 2**np.mgrid[1:n+1, 1:n+1]
+#     return d
+#
+# print(funkcja(4))
+
+# #zad4
+# def potegi(n, m):
+#     e = np.logspace(start=1,num=m, stop=m, base=n, dtype='int')
+#     return e
+# print(potegi(2, 4))
+
+
